c00c154f855410fcffa031c06f9cb242db80dac2
SuhridS <suhridsubramaniam@gmail.com>
2020-03-11 00:24:49 -0700

    changed demo files

--- Analysis/Reddit/reddit_data_scraping.py
+++ Analysis/Reddit/reddit_data_scraping.py
@@ -1,6 +1,5 @@
 #Upload the mastodon_topics.csv file which Mo sent
 import pandas as pd
-!pip install praw
 import praw
 reddit = praw.Reddit(client_id='JEISNEQ9UJHjHA', client_secret='CY4kBtqKfivAxAiFa_9P0YdRhbY', user_agent='Subro')
 
@@ -15,6 +14,8 @@ reddit = praw.Reddit(client_id='JEISNEQ9UJHjHA', client_secret='CY4kBtqKfivAxAiF
 # print(posts)
 
 def generate_reddit_without_user():
+  reddit = praw.Reddit(client_id='JEISNEQ9UJHjHA', client_secret='CY4kBtqKfivAxAiFa_9P0YdRhbY', user_agent='Subro')
+
   filtering = pd.read_csv('mastodon_counts.csv')
   new_filt = filtering.sort_values(by='userCount', ascending=False)
   subreddit_all = list(new_filt['topic'])[33:35]
@@ -36,13 +37,15 @@ def generate_reddit_without_user():
         continue
 
 def generate_reddit_without_user():
+  # reddit = praw.Reddit(client_id='JEISNEQ9UJHjHA', client_secret='CY4kBtqKfivAxAiFa_9P0YdRhbY', user_agent='Subro')
   subreddit_all = list(set(list(pd.read_csv('reddit_data_without_users.csv')['subreddit'])))
+  subreddit_all.SentimentText=subreddit_all.SentimentText.astype(str)
 # print(subreddit_all)
-user_data = []
+  user_data = []
 
-for i in subreddit_all:
-  print(i)
-  subreddit = reddit.subreddit(i)
-  user_data.append([i,subreddit.subscribers,subreddit.accounts_active,subreddit.accounts_active_is_fuzzed,subreddit.active_user_count])
-user_data = pd.DataFrame(user_data, columns=['Subreddit name','Number of subscribers','Active accounts','Active Fuzzed accounts','Active user count'])
-user_data.to_csv('reddit_data_with_user.csv', mode = 'w')
+  for i in subreddit_all:
+    print(i)
+    subreddit = reddit.subreddit(i)
+    user_data.append([i,subreddit.subscribers,subreddit.accounts_active,subreddit.accounts_active_is_fuzzed,subreddit.active_user_count])
+  user_data = pd.DataFrame(user_data, columns=['Subreddit name','Number of subscribers','Active accounts','Active Fuzzed accounts','Active user count'])
+  user_data.to_csv('reddit_data_with_user.csv', mode = 'w')
